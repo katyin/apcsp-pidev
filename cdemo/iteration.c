18cd27a168b542bc4b924df0171232ba0ede2659
katyin <38962446+katyin@users.noreply.github.com>
2018-05-27 16:28:13 -0700

    Update iteration.c

--- cdemo/iteration.c
+++ cdemo/iteration.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
-
 int main()
 {
+
 	int div = 5
+
 	// standard for loop - from 0
-	for (int i = 0; i < 100; i++)
+
+;	for (int i = 0; i < 100; i++)
+
 	{
-		if (i%div == 0_
-			printf("i is equal to 0")
+
+		if (i%div == 0)
+
+			printf("i is equal to %d\n", i);
+
 	}
+
 }
