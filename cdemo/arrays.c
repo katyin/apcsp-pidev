2856a830f0b5d9366782bec5cc4a6f5c01989f7d
katyin <38962446+katyin@users.noreply.github.com>
2018-05-28 21:46:42 -0700

    Update arrays.c

--- cdemo/arrays.c
+++ cdemo/arrays.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
+void arrayAdd(int* arrPtr, int s, int n);
 int main()
 {
   int arr[100];
   int i;
+  int n;
+  int s;
 for (i = 0; i <100; i++)
 {
   arr[i] = i*i;
 }
-int* arrPtr = &arr[0]
-printf("Enter the amount to increment each element:")
-scanf("%d\n", &n)
-int arrayadd;
-for (i = 0; i <100; i++)
+int* arrPtr = &arr[0];
+arrayAdd(arrPtr, s, n);
+printf("Enter the amount to increment each element:");
+scanf("%d\n", &n);
+for (i = 0; i <100; i++);
 {
-  printf("d%\n", *arr++);
+  printf("d%\n", *arrPtr++);
 }
 
-};
+}
 
 
 //void because we don't want a return
@@ -25,7 +28,8 @@ void arrayAdd(int *arrPtr, int s, int n)
 //n is amount each element increases
 // arr is pointer for array
 {
-for (i = 0; i < s; i++)
+  int i;
+  for (i = 0; i < s; i++);
 {
   *arrPtr = *arrPtr + n;
   arrPtr++;
