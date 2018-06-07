b459c57fac9877d3a635ba1b8349ddca6d9a2034
Katyin <katherinebyin@gmail.com>
2018-06-07 16:01:30 +0000

    first commit

--- cdemo/areaOfCircle.c
+++ cdemo/areaOfCircle.c
@@ -7,14 +7,17 @@ int main()
   printf("Enter Radius:");
   scanf("%f", &radius);
   area = areaOfCircle(radius);
-  printf("Area=%f\n", area);
+    printf("Radius = %d Area = %f\n", areaOfCircle(radius));
 }
 
 float areaOfCircle(float radius)
+{
+
+    for (float radius = 3.5; radius < 12.6; radius ++)
 {
 //calculating for area//
 float area;
 area = 3.14*radius*radius;
 return (area);
 }
-
+}
--- cdemo/userRadiusArea.c
+++ cdemo/userRadiusArea.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 float areaOfCircle (float radius);
-int main()
+int main(int argc, char*argv[])
 {
+  int arg1;
+  int arg2;
   float radius[256];
   float area;
   char input1[256];
   char input2[256];
-  int age;
-  
-  printf("Enter a range for the radius?\n");
-  fgets(input1, 256, stdin);
-  sscanf(input1, "%s", radius);
-  fgets(input2, 256, stdin);
-  sscanf(input2, "%s", radius);
-
-  printf("%s, %s\n", radius);
-  while (1)
-  {
-    fgets(input, 256, stdin);
-    if (sscanf(input, "%d", &age) == 1) break;
-    printf("Not a valid age - try again!\n");
-  }
-  
-  printf("Hi %s, you look young for %d…\n", name, age);
-  printf("Enter Radius:");
-  scanf("%f", &radius);
-  area = areaOfCircle(radius);
-  printf("Area=%f\n", area);
+  int num1;
+  int num2;
+  float area(float a)
+{
+   return 3.14*a*a;
 }
 
+
+printf("Enter a range for the radius?\n");
+fgets(input1, 256, stdin);
+  if (sscanf(input1, "%d", &num1) != 1);
+   {
+        printf("Not a valid number\n");
+        return 1;
+   }
+fgets(input2, 256, stdin);
+   if (sscanf(input2, "%d", &num2);
+   {
+	printf("Not a valid number\n");
+	return 1;
+   }
 float areaOfCircle(float radius)
 {//calculating for area//
 for (radius = 3.5; radius <= 12.5; radius++)
