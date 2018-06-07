9c8fd9aab1a2cc9efb3862e16e58f3ede680259d
katyin <38962446+katyin@users.noreply.github.com>
2018-06-07 13:32:40 -0700

    Create strings.c

---
+++ cdemo/strings.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+
+  char str1[] = "abcdefghijklmnopqrstuvwxyz";
+  char str2[27];
+  char c = 'a';
+for (int i = 0; i < 27; i++)
+{
+        str2[i] = c;
+        c++;
+}
+  str2[26]='\0';
+
+  if (strcmp(str1, str2) == 0)
+   { printf("the strings are identical\n");}
+  else
+    {printf("the strings are different\n");}
+printf("string 1:%s\n", str1);
+printf("string 2:");
+for (int i = 0; i < 26; i++)
+{
+        c = str2[i];
+        str2[i] = c - 32;
+        printf("%c", str2[i]);
+}
+printf("\n");
+  if (strcmp(str1, str2) == 0)
+   { printf("the strings are identical\n");}
+  else
+   { printf("the strings are different\n");}
+char str3[52];
+strcat(str3, str1);
+strcat(str3, str2);
+printf("string 3:%s\n", str3);
+}
