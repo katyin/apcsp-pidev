9faff4faadfb297bcab02e69c3639697f8021ce5
katyin <38962446+katyin@users.noreply.github.com>
2018-06-07 08:59:03 -0700

    String

--- cdemo/string.c
+++ cdemo/string.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <string.h>
 int main()
@@ -8,25 +9,30 @@ int main()
   char c = 'a';
 for (int i = 0; i < 27; i++)
 {
-	str2[i] = c;
-	c++;
+        str2[i] = c;
+        c++;
 }
   str2[26]='\0';
-{
+
   if (strcmp(str1, str2) == 0)
-    printf("the strings are identical\n");
+   { printf("the strings are identical\n");}
   else
-    printf("the strings are different\n");
-}
-for (int i = 0; i < 27; i++)
+    {printf("the strings are different\n");}
+printf("string 1:%s\n", str1);
+printf("string 2:");
+for (int i = 0; i < 26; i++)
 {
-	c = str2[i];
-	str2[i] = c - 32;
+        c = str2[i];
+        str2[i] = c - 32;
+        printf("%c", str2[i]);
 }
-{
+printf("\n");
   if (strcmp(str1, str2) == 0)
-    printf("the strings are identical\n");
+   { printf("the strings are identical\n");}
   else
-    printf("the strings are different\n");
-}
+   { printf("the strings are different\n");}
+char str3[52];
+strcat(str3, str1);
+strcat(str3, str2);
+printf("string 3:%s\n", str3);
 }
