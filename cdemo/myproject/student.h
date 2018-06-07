a6871d3690a4d77024af6ab16d530ac40ad3f828
katyin <38962446+katyin@users.noreply.github.com>
2018-06-06 18:43:31 -0700

    Create student.h

---
+++ cdemo/myproject/student.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+struct Student 
+        {
+          char firstname[50];
+          char lastname[50];
+          char age[50];
+          int studentid[100];
+        };
+void printStudents(struct Student stdntstruct[50], int studentnum);
